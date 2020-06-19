253273233940fea47f972a4fb651f7282af95f6a
Mariusz <mar.lewandowski@gmail.com>
2020-06-19 08:39:24 +0200

    New version

--- index.js
+++ index.js
@@ -1,84 +1,60 @@
 //Baza zadań
-const baseOfTasks = ['Umyć zęby', 'Odrobić lekcje', 'Poukładać puzzle'];
-let filteredTasks = baseOfTasks;
-// Usuwanie zadań
+let baseOfTasks = [
+  { id: 0, text: 'Umyć zęby' },
+  { id: 1, text: 'Odrobić lekcje' },
+  { id: 2, text: 'Poukładać puzzle' },
+  { id: 3, text: "Kolejne zadanie" },
+  { id: 4, text: 'jeszcze jedno zadanie' }];
+let filteredTasks;
 
-const deleteTask = e => {
-  e.preventDefault()
-  const indexKiiToDelete = e.target.dataset.kii;
-  baseOfTasks.splice(indexKiiToDelete, 1);
+//Usuwanie zadań
+
+const deleteTask = (e) => {
+  const index = e.target.className
+  baseOfTasks.splice(index, 1)
+  const newBaseOfTasks = [];
+  let id = 0;
+  baseOfTasks.forEach(task => {
+    newBaseOfTasks.push({ id, text: task.text });
+    id++;
+  })
+  console.log(newBaseOfTasks)
+  baseOfTasks = newBaseOfTasks;
   printTasks()
-};
 
-//Wyświetlanie zadań
-const ul = document.querySelector('ul');
+  // console.log()
+}
+
+// Wyświetlanie zadań
 
 const printTasks = () => {
+  const ul = document.querySelector('ul');
   ul.textContent = "";
-  let numberOfTask = 0;
   baseOfTasks.forEach(task => {
     const li = document.createElement('li');
-    li.textContent = task;
-    const btnDelete = document.createElement('button');
-    btnDelete.textContent = "Usuń";
-    btnDelete.dataset.kii = numberOfTask;
-    li.dataset.kii = numberOfTask;
-    numberOfTask++;
-    li.appendChild(btnDelete)
+    const btn = document.createElement('button');
+    li.textContent = task.text;
+    btn.textContent = 'Usuń';
+    btn.classList.add(task.id)
+    li.appendChild(btn)
     ul.appendChild(li)
-    // nasłuchiwanie na usuń
-    document.querySelectorAll('button[data-kii]').forEach(btn => btn.addEventListener('click', deleteTask))
-  });
+    //nasłuchiwanie do usuwania zadań
+    btn.addEventListener('click', deleteTask)
+  })
 }
-
 printTasks()
 
-//Dodawanie zadań
+// Dodawanie zadań
 
-const inputAdd = document.querySelector('input.addTask');
+
 const form = document.querySelector('form');
-
 const addTask = e => {
   e.preventDefault()
-  const newTaskToAdd = inputAdd.value;
-  baseOfTasks.push(newTaskToAdd)
-  printTasks();
-  inputAdd.value = "";
+  const task = e.target[0].value;
+  baseOfTasks[baseOfTasks.length] = { id: baseOfTasks.length, text: task }
+  printTasks()
+  e.target[0].value = "";
+  console.log(document.querySelector('input.filter').value = '');
 }
-
-form.addEventListener('submit', addTask)
-
-
-
-
-
-//Filtrowanie zadań
-
-const printFilteredTasks = () => {
-  ul.textContent = "";
-  let numberOfTask = 0;
-  filteredTasks.forEach(task => {
-    const li = document.createElement('li');
-    li.textContent = task;
-    const btnDelete = document.createElement('button');
-    btnDelete.textContent = "Usuń";
-    btnDelete.dataset.kii = numberOfTask;
-    li.dataset.kii = numberOfTask;
-    numberOfTask++;
-    li.appendChild(btnDelete)
-    ul.appendChild(li)
-    // nasłuchiwanie na usuń
-    document.querySelectorAll('button[data-kii]').forEach(btn => btn.addEventListener('click', deleteTask))
-  });
-}
-
-const inputFilter = document.querySelector('input.filter')
-const filterTasks = () => {
-  const searchPhrase = inputFilter.value.toLowerCase();
-  searchPhrase ? printTasks() : null
-  filteredTasks = baseOfTasks.filter(task => task.toLowerCase().includes(searchPhrase));
-  printFilteredTasks()
-
-}
-inputFilter.addEventListener('input', filterTasks)
+form.addEventListener('submit', addTask);
 
